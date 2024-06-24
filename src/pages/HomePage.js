0065ad816aec5feb46f3250b778c2bb360afdf9c
Amber Sautner <ambersautner3@gmail.com>
2024-06-24 13:17:21 -0400

    dependency

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,6 @@
 // src/HomePage.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Link } from 'react-router-dom';
 import IVTherapy from '../components/Infographics';
 import '../css/homepage.css';
 
