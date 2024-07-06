49901165d65e4d5c970a68c445b9f11542271292
Amber Sautner <ambersautner3@gmail.com>
2024-07-06 16:07:29 -0400

    migraine

--- src/pages/treatments/migraine.js
+++ src/pages/treatments/migraine.js
@@ -5,14 +5,13 @@ const Migraine = () => {
   return (
     <div className="container">
       <div className="image-container">
-        <img src="../images/services/gym.png" alt="Athlete IV" /> {/* Replace with the actual image path */}
+        <img src="../images/services/migraine.jpg" alt="Athlete IV" /> {/* Replace with the actual image path */}
       </div>
       <div className="content-container">
-        <h1>ATHLETE RECOVERY</h1>
-        <h2>b12, magnesium, sodium chloride</h2>
+        <h1>MIGRAIN FIGHTER</h1>
+        <h2>b-plex, pyridoxine, sodium chloride</h2>
         <p>
-        Recovering from a workout or gearing up for a competition? This service is for you, amino acids and B12 help with muscle conditioning, recovery and endurance. This blend also assists in our bodies nervous system, metabolism and protein synthesis. 
-        </p>
+        This multifaceted infusion will improve vascular function linked to migraine attacks and reduces nausea.</p>
   
         <div className="button-container">
           <button className="book-now">Book Here</button>
@@ -23,18 +22,18 @@ const Migraine = () => {
         <div className="ingredient-wrapper">
           <div className="ingredient-container">
             <img src="../images/services/smile.png" alt="B-COMPLEX" />
-            <h1>B-COMPLEX</h1>
-            <h3><i> enables the release of energy through metabolism and supports nervous system</i></h3>
+            <h1>B-PLEX</h1>
+            <h3><i> enables the release of energy through metabolism and supports the nervous system</i></h3>
           </div>
           <div className="ingredient-container">
             <img src="../images/services/lightbulb.png" alt="MAGNESIUM" />
-            <h1>MAGNESIUM</h1>
-            <h3><i>for nerve and muscle function, mood, natural relaxant</i></h3>
+            <h1>PYRIDOXINE</h1>
+            <h3><i>for brain development and keeping the nervous system healthy</i></h3>
           </div>
           <div className="ingredient-container">
             <img src="../images/services/waterdrop.png" alt="B12" /> {/* Replace with the actual image path */}
-            <h1>B12</h1>
-            <h3><i>for cell metabolism, nerve function, and the creation of red blood cells</i></h3>
+            <h1>SODIUM CHLORIDE</h1>
+            <h3><i>for the absorption and transportation of nutrients</i></h3>
           </div>
         </div>
       </div>
@@ -44,21 +43,17 @@ const Migraine = () => {
       </div>
 
       <div className="additional-info">
-          <h1>Benefits of Athlete Recovery</h1>
+          <h1>Benefits of Migraine recovery</h1>
           <ul>
-            <li>Aids recovery</li>
-            <li>Improved endurance</li>
-            <li>Helps with muscle conditioning</li>
+            <li>Reduces nausea</li>
+            <li>Improves vascular function</li>
           </ul>
-          <h1>Athlete Recovery IV Therapy</h1>
+          <h1>Migraine recovery IV Therapy</h1>
           <p>
-          Athlete recovery IV therapy combines essential nutrients such as magnesium, vitamin B12, and B complex vitamins to support rapid recovery and enhance performance. This specialized IV treatment delivers these nutrients directly into the bloodstream, bypassing the digestive system for immediate absorption and effectiveness. 
-          </p>
-          <p>Magnesium plays a crucial role in muscle function and relaxation, which is essential for athletes recovering from intense physical activity. Vitamin B12 supports red blood cell production, aids in energy metabolism, and helps maintain neurological function, all of which are vital for optimal athletic performance. B complex vitamins further contribute to energy production and help in the synthesis of new cells.
-          </p>
+          Migraine recovery IV therapy combines essential nutrients such as B-Plex (Vitamin B Complex), Pyridoxine, and Sodium Chloride to support rapid relief from migraine symptoms and enhance overall recovery. This specialized IV treatment delivers these nutrients directly into the bloodstream, bypassing the digestive system for immediate absorption and effectiveness.</p>
+          <p>B-Plex vitamins play a crucial role in maintaining neurological function and reducing inflammation, which is essential for individuals suffering from migraines. Pyridoxine (Vitamin B6) supports neurotransmitter function, aids in reducing the frequency and severity of migraines, and helps maintain overall neurological health. Sodium Chloride ensures proper hydration, which is vital for alleviating migraine symptoms and preventing future occurrences.</p>
           <p>
-          This targeted IV therapy is particularly beneficial for athletes and active individuals who need quick replenishment of fluids and nutrients to combat dehydration and fatigue. By ensuring rapid hydration and delivering key nutrients directly where they are needed most, athlete recovery IV therapy supports overall wellness, enhances physical recovery, and promotes mental clarity. Regular sessions can contribute to sustained energy levels, improved recovery times, and overall better performance, making it a valuable investment in maintaining peak health and quality of life.
-          </p>
+          This targeted IV therapy is particularly beneficial for individuals seeking quick relief from migraine pain and related symptoms. By ensuring rapid hydration and delivering key nutrients directly where they are needed most, migraine recovery IV therapy supports effective symptom management, improves energy levels, and promotes mental clarity. Regular sessions can contribute to reduced migraine frequency, quicker recovery times, and overall better health, making it a valuable investment in achieving and maintaining peak physical well-being and quality of life.</p>
          
           <h1>Why Choose Us for IV Hydration?</h1>
           <p>
