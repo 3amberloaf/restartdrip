3b77b4335dca3c7502aae6e82601251702f6c305
Amber Sautner <ambersautner3@gmail.com>
2024-07-21 11:22:02 -0400

    improved

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Steps from '../components/steps';
-import '../css/homepage.css';
 import Reviews from '../components/reviews';
 import TreatmentsCarousel from '../components/carousel';
+import '../css/homepage.css';
 
 const HomePage = () => {
   return (
     <main className="main-content">
-      <div className="homepage-section">
+      <div
+        className="homepage-section"
+        style={{
+          backgroundImage: 'url(/images/homepage/homepage_background.png)',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '100vh', // Full viewport height
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}
+      >
+        <div className="homepage-logo">
+          <img src="/images/logo.png" alt="logo" />
+        </div>
         <div className="homepage-text">
-          <h1>Restart Drip</h1>
-          <h2>IV therapy + vitamin injections + NAD+wellness • illness • recovery • beauty</h2>
+          <h3>IV therapy + vitamin injections + NAD+</h3>
+          <h3>wellness • illness • recovery • beauty</h3>
           <div className="homepage-buttons">
             <Link to="/services">
-              <button className="view-services">View All Services</button>
+              <button className="view-services">Book Now</button>
             </Link>
-          </div>
-        </div>
-        <div className="homepage-image-container">
-          <div className="homepage-image">
-            <img src="/images/homepage/homepage_background.png" alt="Homepage" />
           </div>
         </div>
       </div>
