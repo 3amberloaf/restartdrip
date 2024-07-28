344a511434ad0fcd4f61c050fe02c4d00f95b725
Amber Sautner <ambersautner3@gmail.com>
2024-07-28 15:00:50 -0400

    sss

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,8 +7,9 @@ const About = () => {
     <div className="about-us-container">
       <section className="hero-section">
         <div className="hero-content">
-          <h1>About Restart Drip</h1>
-          <p>Dedicated to delivering exceptional in-home IV therapy and medical services.</p>
+          <img src="/images/random/ivbag.png" alt="iv-bag" />
+          <h1>Restart Drip</h1>
+    
         </div>
       </section>
       
@@ -18,12 +19,12 @@ const About = () => {
           At Restart Drip, our mission is to empower individuals to achieve optimal health and well-being through personalized IV hydration therapy. As a woman-owned business, we are committed to delivering exceptional care with a focus on integrity, compassion, and excellence. We strive to create a supportive and welcoming environment where our clients can rejuvenate and thrive. Our goal is to inspire and uplift our community by promoting wellness from within, fostering a culture of health, and making a positive impact on the lives we touch.
         </p>
           <div className="nurse-image">
-            <img src="images/about/nurse.png" alt="About" />
+            <img src="images/homepage/droplet.png" alt="About" />
           </div>
       </section>
 
       <div className="our-founder">
-        <h2>Meet Our Founder</h2>
+        <h2>Our Founder</h2>
         <div className="founder">
           <img src="images/homepage/founder.png" alt="Founder" className="founder-image" />
           <div className="founder-info">
