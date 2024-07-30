bc137ff0d25d8ddf4aa2dc6ddd5726fec66c6652
Amber Sautner <ambersautner3@gmail.com>
2024-07-30 15:11:41 -0400

    iv therapy animations

--- src/components/IVTherapy.js
+++ src/components/IVTherapy.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import '../css/IVTherapy.css';
 
 const IVTherapy = () => {
@@ -8,68 +8,28 @@ const IVTherapy = () => {
         setActiveSection(activeSection === section ? null : section);
     };
 
-    useEffect(() => {
-        const elements = document.querySelectorAll(".additional-info p, .additional-info li, .additional-info ul, .additional-info h1");
-    
-        const elementInView = (element, offset = 0) => {
-          const elementTop = element.getBoundingClientRect().top;
-          return (
-            elementTop <= ((window.innerHeight || document.documentElement.clientHeight) - offset)
-          );
-        };
-    
-        const displayScrollElement = (element) => {
-          element.classList.add('visible-text');
-        };
-    
-        const hideScrollElement = (element) => {
-          element.classList.remove('visible-text');
-        };
-    
-        const handleScrollAnimation = () => {
-          elements.forEach((el) => {
-            if (elementInView(el, 100)) {
-              displayScrollElement(el);
-            } else {
-              hideScrollElement(el);
-            }
-          });
-        };
-    
-        window.addEventListener('scroll', handleScrollAnimation);
-    
-        handleScrollAnimation();
-    
-    
-        return () => {
-          window.removeEventListener('scroll', handleScrollAnimation);
-        };
-      }, []);
-
     return (
-  
         <div className="iv-therapy">
             <div className='iv-therapy-header'>
                 <h1>IV THERAPY & INJECTION SERVICES</h1>
             </div>
-        <div className="iv-therapy-container">
-            <div className="iv-therapy-image-text">
-                <h3>RELAX - RECHARGE - REHYDRATE</h3>
-                <p>
-                New to IV therapy? At Restart Drip, we will help you <span style={{ fontWeight: 'bold', fontStyle: 'italic' }}>RESTART</span> your life at 100%!
-            </p>
-                <img src="./images/homepage/iv_homepage.png" alt="Restart Drip" className="iv-therapy-image" />
-            </div>
-            <div className="iv-therapy-section">
-                <div className="service-list">
-                    {['whatIsIt?', 'whatAreTheBenefits?', 'howDoesItWork?', 'whoCanBenefit?', 'whatShouldIExpect?', 'learnMore!'].map(section => (
-                        <div key={section}>
-                            <button className="service-button" onClick={() => toggleSection(section)}>
-                                {section.split(/(?=[A-Z])/).join(" ").toUpperCase()}
-                                <span className={`icon ${activeSection === section ? 'open' : ''}`}>&#9660;</span>
-                            </button>
-                            {activeSection === section && (
-                                <div className="service-content">
+            <div className="iv-therapy-container">
+                <div className="iv-therapy-image-text">
+                    <h3>RELAX - RECHARGE - REHYDRATE</h3>
+                    <p>
+                        New to IV therapy? At Restart Drip, we will help you <span style={{ fontWeight: 'bold', fontStyle: 'italic' }}>RESTART</span> your life at 100%!
+                    </p>
+                    <img src="./images/homepage/iv_homepage.png" alt="Restart Drip" className="iv-therapy-image" />
+                </div>
+                <div className="iv-therapy-section">
+                    <div className="service-list">
+                        {['whatIsIt?', 'whatAreTheBenefits?', 'howDoesItWork?', 'whoCanBenefit?', 'whatShouldIExpect?', 'learnMore!'].map(section => (
+                            <div key={section}>
+                                <button className="service-button" onClick={() => toggleSection(section)}>
+                                    {section.split(/(?=[A-Z])/).join(" ").toUpperCase()}
+                                    <span className={`icon ${activeSection === section ? 'open' : ''}`}>&#9660;</span>
+                                </button>
+                                <div className={`service-content ${activeSection === section ? 'open' : ''}`}>
                                     {section === 'whatIsIt?' && (
                                         <p>
                                             Intravenous (IV) therapy involves delivering fluids, vitamins, minerals, and other nutrients directly into the bloodstream through a vein. It's a medical treatment commonly used to rehydrate the body, replenish essential nutrients, and address various health concerns.
@@ -112,12 +72,11 @@ const IVTherapy = () => {
                                         </p>
                                     )}
                                 </div>
-                            )}
-                        </div>
-                    ))}
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     );
 };
